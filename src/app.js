3979174b1cd89e8f41dd430fa1f490ad0a754138
Alok905 <alokranjanjoshi07567@gmail.com>
2022-10-09 22:07:41 +0530

    appended start in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
